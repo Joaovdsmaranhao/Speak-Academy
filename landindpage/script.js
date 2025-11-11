a78267cce4608582b16add31d1cbb92513e82a94
João Vitor Maranhão <joaovitordsmaranhao@gmail.com>
2025-11-11 20:07:59 -0300

    acertos na responsividade e no menu mobile

--- landindpage/script.js
+++ landindpage/script.js
@@ -1,30 +1,23 @@
-const navbar = document.getElementById('navbar')
-const logoacademy = document.getElementById('academy')
-const navlinks = document.querySelectorAll('.nav-links a')
+
 const mobilemenu = document.getElementById('menu-mobile')
 const mobilelinks = document.getElementById('menu-links')
 const btninscrever = document.getElementById('mobile-btn')
-
-window.addEventListener('scroll', () => {
-    if(window.scrollY > 100) {
-        navbar.classList.add("scrolled")
-        logoacademy.classList.add("scrolled")
-        navlinks.forEach(links =>
-            links.classList.add("scrolled")
-        )
-    }
-    else  {
-        navbar.classList.remove("scrolled");
-        logoacademy.classList.remove("scrolled")
-        navlinks.forEach(links =>
-            links.classList.remove("scrolled")
-        )
-    }
-})
+const navbarr = document.getElementById('navbar')
+const menubar = document.getElementById('icone')
 
 
 mobilemenu.addEventListener('click',() => {
+
     mobilelinks.classList.toggle('activate')
     btninscrever.classList.toggle('activate')
+    navbarr.classList.toggle('activate')
+    if (menubar.classList.contains('bi-list')) {
+
+        menubar.classList.replace('bi-list', 'bi-x')
+    }
+    else {
+        menubar.classList.replace('bi-x','bi-list')
+    }
+
 })
 
